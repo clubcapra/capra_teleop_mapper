92e5fedbaf75965a88a2195f10bbfcbe0f45fa97
IliTheButterfly <iliana.decarufel@gmail.com>
2026-01-12 14:29:46 -0500

    The node was doing the absolute opposite of its purpose. The node will now take a single joy topic and split it into two

--- src/teleop_mapper_node.cpp
+++ src/teleop_mapper_node.cpp
@@ -8,24 +8,24 @@ class TeleopMapper : public rclcpp::Node
 {
 public:
   TeleopMapper()
-  : Node("teleop_mapper"), selected_(1)
+  : Node("teleop_mapper"), selected_(false)
   {
-    joy_pub_ = this->create_publisher<sensor_msgs::msg::Joy>(
-      "/joy", rclcpp::QoS(10));
+    joy1_pub_ = this->create_publisher<sensor_msgs::msg::Joy>(
+      "/joy1", rclcpp::QoS(10));
 
-    joy1_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
-      "/joy1", rclcpp::QoS(10),
-      std::bind(&TeleopMapper::joy1_cb, this, _1));
+    joy2_pub_ = this->create_publisher<sensor_msgs::msg::Joy>(
+      "/joy2", rclcpp::QoS(10));
 
-    joy2_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
-      "/joy2", rclcpp::QoS(10),
-      std::bind(&TeleopMapper::joy2_cb, this, _1));
+    joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
+      "/joy", rclcpp::QoS(10),
+      std::bind(&TeleopMapper::joy_cb, this, _1));
 
     select_sub_ = this->create_subscription<std_msgs::msg::Bool>(
       "/joy_select", rclcpp::QoS(10),
       std::bind(&TeleopMapper::select_cb, this, _1));
 
-    RCLCPP_INFO(this->get_logger(), "Joy mux started (default: joy1)");
+    RCLCPP_INFO(this->get_logger(),
+                "Joy demux started (false→/joy1, true→/joy2)");
   }
 
 private:
@@ -34,25 +34,20 @@ private:
     selected_ = msg->data;
   }
 
-  void joy1_cb(const sensor_msgs::msg::Joy::SharedPtr msg)
-  {
-    if (!selected_) {
-      joy_pub_->publish(*msg);
-    }
-  }
-
-  void joy2_cb(const sensor_msgs::msg::Joy::SharedPtr msg)
+  void joy_cb(const sensor_msgs::msg::Joy::SharedPtr msg)
   {
     if (selected_) {
-      joy_pub_->publish(*msg);
+      joy2_pub_->publish(*msg);
+    } else {
+      joy1_pub_->publish(*msg);
     }
   }
 
   bool selected_;
 
-  rclcpp::Publisher<sensor_msgs::msg::Joy>::SharedPtr joy_pub_;
-  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy1_sub_;
-  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy2_sub_;
+  rclcpp::Publisher<sensor_msgs::msg::Joy>::SharedPtr joy1_pub_;
+  rclcpp::Publisher<sensor_msgs::msg::Joy>::SharedPtr joy2_pub_;
+  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
   rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr select_sub_;
 };
 
